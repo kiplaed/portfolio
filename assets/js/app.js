e0159876960e399285e98a728b3e3f3c7c3f2ac9
vybekid1 <77786099+vybekid1@users.noreply.github.com>
2022-10-19 12:04:53 +0300

    new changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,20 +30,3 @@ navItems.forEach((item) => {
           item.classList.add("active");
      };
 });
-
-// let navbarlinks = select('.navbar-link', true);
-// const navbarlinksActive = () => {
-//   let position = window.scrollY + 200;
-//   navbarlinks.forEach(navbarlink => {
-//     if (!navbarlink.hash) return;
-//     let section = select(navbarlink.hash);
-//     if (!section) return;
-//     if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-//       navbarlink.classList.add('active');
-//     } else {
-//       navbarlink.classList.remove('active');
-//     }
-//   });
-// };
-// window.addEventListener('load', navbarlinksActive);
-// onscroll(document, navbarlinksActive);
